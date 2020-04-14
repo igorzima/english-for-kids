a2d5d523c31832d516ae9f4527ab518f82d6b1c0
igorzima <redteam122@gmail.com>
2020-04-14 13:02:39 +0300

    feat: add new page generation

--- src/script/index.js
+++ src/script/index.js
@@ -5,6 +5,8 @@ import createCards from './Card';
 
 const HAMBURGER = document.getElementById('hamburger');
 const MENU = document.getElementById('menu');
+const NAVIGATION = document.getElementById('navigation');
+
 
 
 HAMBURGER.addEventListener('click', () => {
@@ -19,52 +21,155 @@ HAMBURGER.addEventListener('click', () => {
   }
 })
 
-const NAVIGATION = document.getElementById('navigation');
 
 function createNavigation(arr) {
-  let li = `<li class='navigation__item'>Main Pagen</li>`;
-
+  let li = `<li class='navigation__item'>Main Page</li>`;
+  
   arr[0].map((e) => {
     li += `<li class='navigation__item'>${e}</li>`;
     return li;
   })
+
   NAVIGATION.innerHTML = li;
+  const NAVIGATION_LIST = document.querySelectorAll('.navigation__item');
+
+  for(let i = 0; i < NAVIGATION_LIST.length; i += 1) {
+    NAVIGATION_LIST[i].dataset.key = i;
+  }
+}
+
+function createMain(arr) {
+  let div = '';
+  
+  for(let i = 1; i < arr.length; i += 1) {
+    for(let x = 0; x < 1; x += 1) {
+      div += `<div class="main-card train"><img src='${arr[i][x].image}' alt=""><span>${arr[0][i-1]}</span></div>`;
+    }
+  }
+
+  document.getElementById('cards').innerHTML = div;
+  const DIV_MAIN = Array.from(document.getElementsByClassName('main-card'));
+  for(let i = 0; i < DIV_MAIN.length; i += 1) {
+    DIV_MAIN[i].dataset.main = i + 1;
+  }
+}
+
+createMain(cards);
+
+createNavigation(cards);
+
+
+
+function cardListener() {
+  const RETURN = document.querySelectorAll('.card');
+
+  RETURN.forEach((el) => {
+    el.addEventListener('click', (event) => {
+      if(event.target.className === 'icon') {
+        el.classList.add('reverse');
+      }
+    })
+
+    el.addEventListener('mouseleave', () => {
+      el.classList.remove('reverse');
+    })
+  })
+
+  const PLAY = document.querySelectorAll('figure');
+
+  PLAY.forEach((el) => {
+    el.addEventListener('click', (event) => {
+      const audio = document.querySelector(`audio[data-key='${el.className}']`);
+
+      if(!(event.target.className === 'icon')) {
+        audio.play();
+      }
+    })
+  })
 }
 
 
-// document.addEventListener('click', (event) => {console.log(event.target)})
+function mainPage() {
+  const DIV_MAIN = Array.from(document.getElementsByClassName('main-card'));
 
-// const check = document.getElementById('check');
-// function isCheck() {
-//   check.addEventListener('click', () => {
-//   })
-
-createNavigation(cards);
-createCards(cards[1]);
-
-
-const RETURN = document.querySelectorAll('.card');
-
-Array.from(RETURN).map((el) => {
-  el.addEventListener('click', (event) => {
-    if(event.target.className === 'icon') {
-      el.classList.add('reverse');
-    }
+  DIV_MAIN.forEach((el) => {
+    el.addEventListener('click', () => {
+      switch(el.dataset.main) {
+        case  '1':
+          createCards(cards[1]);
+          cardListener();
+          break;
+        case '2':
+          createCards(cards[2]);
+          cardListener();
+          break;
+        case '3':
+          createCards(cards[3]);
+          cardListener();
+          break;
+        case '4':
+          createCards(cards[4]);
+          cardListener();
+          break;
+        case '5':
+          createCards(cards[5]);
+          cardListener();
+          break;
+        case '6':
+          createCards(cards[6]);
+          cardListener();
+          break;
+        default:
+          break;
+      }
+    })
   })
+}
 
-  el.addEventListener('mouseleave', () => {
-    el.classList.remove('reverse');
-  })
-})
+mainPage()
 
-const PLAY = document.querySelectorAll('figure');
 
-Array.from(PLAY).map((el) => {
-  el.addEventListener('click', (event) => {
-    const audio = document.querySelector(`audio[data-key='${el.className}']`);
 
-    if(!(event.target.className === 'icon')) {
-      audio.play();
+
+const NAVIGATION_LIST = document.querySelectorAll('.navigation__item');
+
+NAVIGATION_LIST.forEach((el) => {
+  el.addEventListener('click', () => {
+    MENU.classList.remove('menu_active');
+    MENU.classList.add('menu');
+    HAMBURGER.classList.remove('hamburger_rotate');
+
+    switch(el.dataset.key) {
+      case '0':
+        createMain(cards);
+        mainPage();
+        break;
+      case  '1':
+        createCards(cards[1]);
+        cardListener();
+        break;
+      case '2':
+        createCards(cards[2]);
+        cardListener();
+        break;
+      case '3':
+        createCards(cards[3]);
+        cardListener();
+        break;
+      case '4':
+        createCards(cards[4]);
+        cardListener();
+        break;
+      case '5':
+        createCards(cards[5]);
+        cardListener();
+        break;
+      case '6':
+        createCards(cards[6]);
+        cardListener();
+        break;
+      default:
+        break;
     }
   })
 })
